d492238a25121d273f359719e9385bc00a3d3d73
Nadia Collado <collado.nadia@gmail.com>
2021-06-11 23:49:39 -0400

    created searchbyId and saveBook utilities

---
+++ utils/saveBookToList.js
@@ -0,0 +1,13 @@
+module.exports = async id => {
+  try {
+    // search for book using its ID
+
+    // save details in a readable format for list
+
+    // save book details to reading list txt file
+
+    // respond with book info for user message
+  } catch (error) {
+    console.log(error)
+  }
+};
---
+++ utils/searchById.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+module.exports = async id => {
+  try {
+    // check if id exists and if it's valid, if it doesn't, throw error and return
+
+    // retreive book from Google Books API using ID, return book
+    const book = await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
+  
+    return book
+  } catch (error) {
+    console.log(error)
+  }
+};
