43ebffb6b7cc1b6f3c4317d87cef0d2853f270ca
Nadia Collado <collado.nadia@gmail.com>
2021-06-12 12:41:08 -0400

    updated searchById utility with id check and error message

--- utils/searchById.js
+++ utils/searchById.js
@@ -3,11 +3,12 @@ const axios = require('axios')
 module.exports = async id => {
   try {
     // check if id exists and if it's valid, if it doesn't, throw error and return
+    if (!id || id === undefined || typeof id !== 'string'){
+      throw new Error(`Invalid ID`)
+    }
 
     // retreive book from Google Books API using ID, return book
     const book = await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
-
-    console.log(book)
     return book
   } catch (error) {
     console.log(error)
