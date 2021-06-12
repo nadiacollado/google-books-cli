f5d741cf7945eea279bae2e01c2f10e6c1a7af70
Nadia Collado <collado.nadia@gmail.com>
2021-06-11 23:43:41 -0400

    updated search response with message

--- utils/searchResponse.js
+++ utils/searchResponse.js
@@ -1,5 +1,10 @@
 module.exports = (query, books) => {
   try {
+    console.log(`
+    Below please find 5 books based on your "${query}" search query. \n \n
+    To save a book to your reading list, copy its ID and use it in the following command: \n \n
+    google-books save --id yourBookIDHere
+    `)
 
     books.data.items.map(book => {
       console.log(`
