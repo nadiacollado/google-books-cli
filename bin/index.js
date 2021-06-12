8f2e7f26faf26c06977f75d17953549817ced6f2
Nadia Collado <collado.nadia@gmail.com>
2021-06-11 20:20:54 -0400

    added index.js

---
+++ bin/index.js
@@ -0,0 +1,27 @@
+const minimist = require('minimist')
+
+module.exports = () => {
+  const args = minimist(process.argv.slice(2))
+  
+  let command = args._[0] || 'help'
+
+  if (args.help || args.h) {
+    command = 'help'
+  }
+
+  if (args.version || args.v) {
+    command = 'version'
+  }
+
+  switch(command){
+    case 'version':
+      require('../commands/version')(args)
+      break
+    case 'help':
+      require('../commands/help')(args)
+      break
+    default:
+      console.log(`${command} is not a valid command.`)
+      break
+  }
+}
