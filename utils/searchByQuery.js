02ced7e1f9e9042974013161bc5db6cc260f68d9
Nadia Collado <collado.nadia@gmail.com>
2021-06-13 14:41:25 -0400

    created special error message variable to fix style.error bug

--- utils/searchByQuery.js
+++ utils/searchByQuery.js
@@ -6,11 +6,13 @@ const Style = require('../utils/style')
 module.exports = async query => {
   try {
     const style = new Style()
+    const errorMessage = style.error(`Your query did not yield any results.\n`)
+
     // checks if query exists, if it doesn't, throw error
     if (query === undefined || !query || query === true && query !== 'true') {
-      throw new Error(boxen(style.error(`
-      Your query did not yield any results.\n`)
-      `Please make sure you have correctly formatted your search. The correct format for the search command is as follows: \n
+      throw new Error(boxen(`
+      ${errorMessage}
+      Please make sure you have correctly formatted your search. The correct format for the search command is as follows: \n
       google-books search --query "search query"
       `, style.box))
     }
