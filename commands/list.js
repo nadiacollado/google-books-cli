80153acb82b3790be7fdb60f2caae37537dd13c7
Nadia Collado <collado.nadia@gmail.com>
2021-06-14 23:16:11 -0400

    bug: list test not passing, list returning undefined

--- commands/list.js
+++ commands/list.js
@@ -3,13 +3,17 @@ const boxen = require('boxen')
 const Style = require('../utils/style')
 
 module.exports = async () => {
-  const style = new Style()
-
-  // retrieves book list from text file
-  await fs.readFile('book-list.txt', (err, data) => {
-    if (err) throw err
-    else {
+  try {
+    const style = new Style()
+    
+    // retrieves book list from text file
+    await fs.readFile('book-list.txt', (err, data) => {
+      if (err) throw err
+      else {
       console.log(boxen(`\nReading list: \n\n${data}`, style.box))
-    }
-  })
+      }
+    })
+  } catch (error){
+    console.log(error)
+  }
 }
