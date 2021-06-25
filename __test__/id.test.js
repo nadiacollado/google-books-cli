eab0e0f232fcbd00b30bff858440214e5cb05099
Nadia Collado <collado.nadia@gmail.com>
2021-06-25 18:29:47 -0400

    string check test passing for id test

--- __test__/id.test.js
+++ __test__/id.test.js
@@ -24,8 +24,21 @@ describe('searchById.js', () => {
     const style = new Style()
     const errorMessage = style.error(`Invalid ID.\n`)
     const idError = boxen(`${errorMessage}\nCheck ID spelling. Also, please make sure you have correctly formatted your entry.\nThe correct format for the save command is as follows: \n\ngoogle-books save --id idNumber`, style.box)
+
     const error = await searchById()
+    
+    expect(error.message).toBe(idError)
+    expect(error.name).toBe('Error')
+  })
+
+  it('throws an error if book ID is not a string', async () => {
+    const style = new Style()
+    const errorMessage = style.error(`Invalid ID.\n`)
+    const idError = boxen(`${errorMessage}\nCheck ID spelling. Also, please make sure you have correctly formatted your entry.\nThe correct format for the save command is as follows: \n\ngoogle-books save --id idNumber`, style.box)
+
+    const error = await searchById(55)
 
     expect(error.message).toBe(idError)
+    expect(error.name).toBe('Error')
   })
 })
